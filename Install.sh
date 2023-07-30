3b0cd6787f1d68b9856d32e041817213dd2e8138
LeeBRogers <90856031+LeeBRogers@users.noreply.github.com>
2023-07-30 23:26:09 +0100

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -4,3 +4,4 @@ pacman -S --needed - < Packages/Base.txt
 pacman -S --needed - < Packages/KDE.txt
 #paru -S --needed - < Packages/Cinnamon.txt
 #paru -S --needed - < Packages/XFCE.txt
+#pacman -S --needed - < Packages/GNOME.txt
